5fa59b49681a107103b59a13d2fe5a05e1517283
ming <mick66145@gmail.com>
2024-09-28 08:33:05 +0800

    feat: InputTimeRange

---
+++ src/@core/components/form/InputTimeRange.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row q-col-gutter-md">
+    <div class="col-6">
+      <input-time class="w-full" v-model="observeValue.start_time" />
+    </div>
+    <div class="col-6">
+      <input-time class="w-full" v-model="observeValue.end_time" :min="observeValue.start_time" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent} from 'vue-demi'
+import { useVModel } from '@vueuse/core'
+export default defineComponent({
+  props: {
+    modelValue: { type: Object, default: { start_time: null, end_time: null } },
+  },
+  emits: [
+    'update:modelValue',
+  ],
+  setup(props, { emit }) {
+    // data
+    const observeValue = useVModel(props, 'modelValue', emit)
+
+    return {
+      observeValue,
+    }
+  },
+})
+</script>
+
+<style lang="postcss" scoped></style>
--- src/@core/components/index.js
+++ src/@core/components/index.js
@@ -53,6 +53,7 @@ import InputRadioOptionGroup from './form/InputRadioOptionGroup.vue'
 import InputRadioOtherOptionGroup from './form/InputRadioOtherOptionGroup.vue'
 import InputEsign from './form/InputEsign.vue'
 import InputNumberRange from './form/InputNumberRange.vue'
+import InputTimeRange from './form/InputTimeRange.vue'
 import BaseButton from './button/BaseButton.vue'
 import AddButton from './button/AddButton.vue'
 import DeleteButton from './button/DeleteButton.vue'
@@ -191,6 +192,7 @@ export default {
     app.component('InputRadioOtherOptionGroup', InputRadioOtherOptionGroup)
     app.component('InputEsign', InputEsign)
     app.component('InputNumberRange', InputNumberRange)
+    app.component('InputTimeRange', InputTimeRange)
     app.component('BaseButton', BaseButton)
     app.component('AddButton', AddButton)
     app.component('DeleteButton', DeleteButton)
