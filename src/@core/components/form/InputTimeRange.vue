5bfa94056ee8767a1de148990a245c63abbea862
ming <mick66145@gmail.com>
2024-10-05 21:34:21 +0800

    feat(InputTimeRange): props readonly、disable

--- src/@core/components/form/InputTimeRange.vue
+++ src/@core/components/form/InputTimeRange.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="row q-col-gutter-md">
     <div class="col-6">
-      <input-time v-model="observeValue.start_time" class="w-full" :required="required" />
+      <input-time v-model="observeValue.start_time" class="w-full" :required="required" :readonly="readonly" :disable="disable" />
     </div>
     <div class="col-6">
-      <input-time v-model="observeValue.end_time" class="w-full" :min="observeValue.start_time" :required="required" />
+      <input-time
+        v-model="observeValue.end_time"
+        class="w-full"
+        :min="observeValue.start_time"
+        :required="required"
+        :readonly="readonly"
+        :disable="disable"
+      />
     </div>
   </div>
 </template>
@@ -16,6 +23,8 @@ export default defineComponent({
   props: {
     modelValue: { type: Object, default: { start_time: null, end_time: null } },
     required: { type: Boolean, default: false },
+    readonly: { type: Boolean, default: false },
+    disable: { type: Boolean, default: false },
   },
   emits: [
     'update:modelValue',
