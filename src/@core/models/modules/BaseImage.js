935f17930bc933a7d91019ff9bb217a94ea5c60c
ming <mick66145@gmail.com>
2024-11-27 14:40:11 +0800

    refactor: BaseFile、BaseImage

--- src/@core/models/modules/BaseImage.js
+++ src/@core/models/modules/BaseImage.js
@@ -1,56 +1,12 @@
-import Base from '@core/models/modules/Base'
-import useBatchUpload from '@/hooks/useBatchUpload'
+import BaseMedia from './BaseMedia'
 
-const BaseImage = () => ({
-  ...Base(),
-  // api欄位
-  id: '',
-  title: '',
-  alt: '',
-  mime: '',
-  name: '',
-  size: '',
-  url: '',
-})
+const BaseImageViewModel = (item = null) =>
+  BaseMedia(
+    {
+      key: 'image',
+      extraFields: ['title', 'alt'],
+    },
+    item,
+  )
 
-const BaseImageModel = (item = null) => {
-  const model = (item) => {
-    return {
-      // api欄位
-      id: item?.id || '',
-      title: item?.title || '',
-      alt: item?.alt || '',
-      mime: item?.mime || '',
-      name: item?.name || '',
-      size: item?.size || '',
-      url: item?.url || '',
-
-    }
-  }
-  return model(item || BaseImage())
-}
-
-const BaseImageObjViewModel = (item = null) => {
-  const { batchUpload } = useBatchUpload()
-  const model = (item) => {
-    return {
-      image: item?.image || '',
-      image_obj: item?.image_obj !== undefined ? item?.image_obj : ((item?.image && Object.keys(item?.image).length !== 0) ? { ...BaseImageModel(item?.image) } : ''),
-      async uploadImage () {
-        if (this.image_obj?.raw) {
-          const [uploadRes] = await batchUpload({ imageObj: this.image_obj })
-          if (uploadRes && uploadRes.imageObj) {
-            this.image_obj.id = uploadRes.imageObj.id
-          }
-        }
-      },
-      setImage () {
-        this.image = this.image_obj
-      },
-    }
-  }
-
-  return model(item)
-}
-
-export default BaseImageObjViewModel
+export default BaseImageViewModel
