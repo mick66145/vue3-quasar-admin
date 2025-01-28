dc5f74418413d556083854072d0d018abdab9f49
ming <mick66145@gmail.com>
2025-01-28 14:30:37 +0800

    feat: DetailScrollArea、ListScrollArea

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,15 @@
 import InputRoleSelect from './form/InputRoleSelect.vue'
 import InputCompanyJobSelect from './form/InputCompanyJobSelect.vue'
 import InputDataAccessRoleSelect from './form/InputDataAccessRoleSelect.vue'
+import ListScrollArea from './scroll-area/ListScrollArea.vue'
+import DetailScrollArea from './scroll-area/DetailScrollArea.vue'
 
 export default {
   install (app) {
     app.component('InputRoleSelect', InputRoleSelect)
     app.component('InputCompanyJobSelect', InputCompanyJobSelect)
     app.component('InputDataAccessRoleSelect', InputDataAccessRoleSelect)
+    app.component('ListScrollArea', ListScrollArea)
+    app.component('DetailScrollArea', DetailScrollArea)
   },
 }
---
+++ src/components/scroll-area/DetailScrollArea.vue
@@ -0,0 +1,20 @@
+<template>
+  <base-scroll-area class="pb-4" style="height: calc(100vh - 200px)">
+    <slot name="default" />
+  </base-scroll-area>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+export default defineComponent({
+  props: {
+  },
+  setup () {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="postcss" scoped>
+</style>
